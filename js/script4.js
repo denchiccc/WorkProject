c966d80078efd14d13848dd1c2239c5794c3ea8c
denis <denurchenko1981@gmail.com>
2022-07-05 16:07:41 +0300

    start item

---
+++ js/script4.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// alert('hello');
+// const result = confirm('are your here?');
+// console.log(result);
+// const answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// const answer = [];
+// answer[0] = prompt('what is you name', '');
+// answer[1] = prompt('what is last name', '');
+// answer[2] = prompt('how old are you', '');
+// console.log(answer);
+
+const user = 'Ivan';
+alert(`привет - ${user}`);
